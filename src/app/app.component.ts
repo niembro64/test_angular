37bc090a606aaf2dc3320b7ddfaae78b47e11f26
=Eric Niemeyer <=niemeyer.eric@gmail.com>
2022-06-08 19:32:41 -0400

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,24 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // templateUrl: './app.component.html',
+  template: `<main>
+    <h1>Structural Directives</h1>
+    <!-- <section> -->
+    <section *hideAfter="5000; then placeholder" class="banner primary">
+      <h3>Temp Content</h3>
+      <p>Disappear in 5 seconds</p>
+    </section>
+    <ng-template #placeholder>
+      <section class="banner placeholder">
+        <h3>Second Content</h3>
+        <p>Appear after 5 seconds</p>
+      </section>
+    </ng-template>
+  </main>`,
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'test_angular';
+  // title = 'test_angular';
+  title = 'test angular';
 }
