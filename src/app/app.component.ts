0ea190c095277e47b3323496a98946e508291c36
niemoBroIntegration <49417819+niemoBroIntegration@users.noreply.github.com>
2022-06-08 20:12:54 -0400

    text change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,19 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  // template: `<main>
-  //   <h1>Structural Directives</h1>
-  //   <section *hideAfter="5000; then placeholder" class="banner primary">
-  //     <h3>Start Content</h3>
-  //     <p>Will disappear in 5 seconds</p>
-  //   </section>
-  //   <ng-template #placeholder>
-  //     <section class="banner placeholder">
-  //       <h3>Second Content</h3>
-  //       <p>Appeared after 5 seconds</p>
-  //     </section>
-  //   </ng-template>
-  // </main>`,
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
